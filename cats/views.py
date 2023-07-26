a2a2badd537c5411c4c2fdd81d607045b3406f85
антон ильичев <anton.ilichev.work@gmail.com>
2023-07-26 12:56:39 +0300

    ViewsSet and Routers

--- cats/views.py
+++ cats/views.py
@@ -2,13 +2,17 @@ from rest_framework import generics
 
 from .models import Cat
 from .serializers import CatSerializer
+from rest_framework import viewsets
 
 
-class CatList(generics.ListAPIView):
+class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
-
-
-class CatDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Cat.objects.all()
-    serializer_class = CatSerializer
+# class CatList(generics.ListAPIView):
+#     queryset = Cat.objects.all()
+#     serializer_class = CatSerializer
+#
+#
+# class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Cat.objects.all()
+#     serializer_class = CatSerializer
