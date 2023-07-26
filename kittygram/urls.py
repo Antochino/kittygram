5e0214662c106e3b91af3f02fa05ab3977e9813d
антон ильичев <anton.ilichev.work@gmail.com>
2023-07-26 11:54:50 +0300

    simple-view-classes

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from cats.views import APICat
+from cats.views import APICat, APICatDetail
 
 urlpatterns = [
    path('cats/', APICat.as_view()),
+   path('cats/<int:pk>/', APICatDetail.as_view())
 ]
 
 
